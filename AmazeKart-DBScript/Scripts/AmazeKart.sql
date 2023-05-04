4d8da8dab0bee4c7dc86ee9aa972823c2999c0ed
Jenish <Jenish@JENISH>
2023-05-04 23:43:07 +0530

    Cart table changes

--- AmazeKart-DBScript/Scripts/AmazeKart.sql
+++ AmazeKart-DBScript/Scripts/AmazeKart.sql
@@ -5,13 +5,13 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Cart](
-	[Id] [int] NOT NULL,
+CREATE TABLE [dbo].[CartDetails](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[CustomerId] [int] NOT NULL,
 	[ProductId] [int] NOT NULL,
 	[Quantity] [int] NOT NULL,
-	[CreatedDate] [datetime] NOT NULL,
- CONSTRAINT [PK_Cart] PRIMARY KEY CLUSTERED 
+	[CreatedDate] [datetime2](7) NOT NULL,
+ CONSTRAINT [PK_CartDetails] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
@@ -190,16 +190,20 @@ CREATE TABLE [dbo].[Suppliers](
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[Cart]  WITH CHECK ADD  CONSTRAINT [FK_Cart_Customers] FOREIGN KEY([CustomerId])
+ALTER TABLE [dbo].[CartDetails]  WITH CHECK ADD  CONSTRAINT [FK_CartDetails_Customers] FOREIGN KEY([CustomerId])
 REFERENCES [dbo].[Customers] ([Id])
 GO
-ALTER TABLE [dbo].[Cart] CHECK CONSTRAINT [FK_Cart_Customers]
+
+ALTER TABLE [dbo].[CartDetails] CHECK CONSTRAINT [FK_CartDetails_Customers]
 GO
-ALTER TABLE [dbo].[Cart]  WITH CHECK ADD  CONSTRAINT [FK_Cart_Products] FOREIGN KEY([ProductId])
+
+ALTER TABLE [dbo].[CartDetails]  WITH CHECK ADD  CONSTRAINT [FK_CartDetails_Products] FOREIGN KEY([ProductId])
 REFERENCES [dbo].[Products] ([Id])
 GO
-ALTER TABLE [dbo].[Cart] CHECK CONSTRAINT [FK_Cart_Products]
+
+ALTER TABLE [dbo].[CartDetails] CHECK CONSTRAINT [FK_CartDetails_Products]
 GO
+
 ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerId])
 REFERENCES [dbo].[Customers] ([Id])
 GO
