cf876aff6d89a51184b1653e66b0bd491fad4a54
Jenish <Jenish@JENISH>
2023-05-12 00:16:57 +0530

    Datainsert and select queries

---
+++ AmazeKart-DBScript/Scripts/SelectQueriesAndDataInsert.sql
@@ -0,0 +1,35 @@
+﻿Select * from Categories
+Select * from Sizes
+Select * from Colors
+
+Select * from Products
+Select * from ProductCatalogs
+Select * from ProductDetails
+
+Select * from CartDetails
+Select * from PaymentDetails
+
+Select * from [Orders]
+Select * from OrderDetails
+
+Select * from Suppliers
+Select * from Customers
+
+-- DATA INSERTION
+
+--Insert into Colors values('Red', 1)
+--Insert into Colors values('Black', 1)
+--Insert into Colors values('White', 1)
+
+--Insert into Sizes values('XS', 1)
+--Insert into Sizes values('S', 1)
+--Insert into Sizes values('XXL', 1)
+--Insert into Sizes values('L', 1)
+--Insert into Sizes values('M', 1)
+
+--Insert into ProductDetails values(1,1,1,1)
+--Insert into ProductDetails values(1,2,2,1)
+--Insert into ProductDetails values(1,3,3,1)
+
+--Insert into ProductDetails values(3,1,1,1)
+--Insert into ProductDetails values(3,2,2,1)
